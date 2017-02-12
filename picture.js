6b9d08d75373c4b7249ea457db8c154ffa0c76ed
Calvin Hansen <calvinhansen@Calvins-MacBook-Pro.local>
2017-02-12 18:07:08 -0500

    Improved recognition not really

--- picture.js
+++ picture.js
@@ -79,25 +79,3 @@ function getImages() {
 
     });
 }
-
-function facialRecognition() {
-      var img = document.getElementById('img-holder');
-      var tracker = new tracking.ObjectTracker(['face']);
-      tracker.setStepSize(1.7);
-      tracking.track('#img', tracker);
-      tracker.on('track', function(event) {
-        event.data.forEach(function(rect) {
-          window.plot(rect.x, rect.y, rect.width, rect.height);
-          
-        });
-      });
-      window.plot = function(x, y, w, h) {
-        var rect = document.createElement('div');
-        document.querySelector('.demo-container').appendChild(rect);
-        rect.classList.add('rect');
-        rect.style.width = w + 'px';
-        rect.style.height = h + 'px';
-        rect.style.left = (img.offsetLeft + x) + 'px';
-        rect.style.top = (img.offsetTop + y) + 'px';
-      };
-    };

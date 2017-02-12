df26f9dfa76dde7c2aac598963cebd2694319e02
Calvin Hansen <calvinhansen@Calvins-MacBook-Pro.local>
2017-02-12 12:38:31 -0500

    Changes

--- picture.js
+++ picture.js
@@ -11,6 +11,7 @@ function setup() {
   b = createButton("Take Picture");
   button = createButton("Get Picture"); //document.getElementById("startTrip");
   button.addClass("btn btn-hg btn-primary");
+  facialRecognition();
 }
 
 function draw() {
@@ -22,8 +23,6 @@ function draw() {
   	picture = capture;
   	image(picture, 0, 0);
   });
-  
-
 }
 
 function getImages() {
@@ -32,7 +31,7 @@ function getImages() {
 
         $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
         {
-            tags: "Eiffel Tower",
+            tags: "paris tourist",
             tagmode: "any",
             format: "json"
         },
@@ -40,10 +39,54 @@ function getImages() {
             var rnd = Math.floor(Math.random() * data.items.length);
 
             var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
+            
+            document.getElementById("img").src = image_src;
 
-            $('body').css('background-image', "url('" + image_src + "')");
+      var img = document.getElementById('img');
+      var tracker = new tracking.ObjectTracker(['face']);
+      tracker.setStepSize(1.7);
+      tracking.track('#img', tracker);
+      tracker.on('track', function(event) {
+        event.data.forEach(function(rect) {
+          window.plot(rect.x, rect.y, rect.width, rect.height);
+          
+        });
+      });
+      window.plot = function(x, y, w, h) {
+        var rect = document.createElement('div');
+        document.querySelector('.demo-container').appendChild(rect);
+        rect.classList.add('rect');
+        rect.style.width = w + 'px';
+        rect.style.height = h + 'px';
+        rect.style.left = (img.offsetLeft + x) + 'px';
+        rect.style.top = (img.offsetTop + y) + 'px';
+      };
+            //facialRecognition();
+            //$('body').css('background-image', "url('" + image_src + "')");
 
         });
 
     });
 }
+
+function facialRecognition() {
+      var img = document.getElementById('img-holder');
+      var tracker = new tracking.ObjectTracker(['face']);
+      tracker.setStepSize(1.7);
+      tracking.track('#img', tracker);
+      tracker.on('track', function(event) {
+        event.data.forEach(function(rect) {
+          window.plot(rect.x, rect.y, rect.width, rect.height);
+          
+        });
+      });
+      window.plot = function(x, y, w, h) {
+        var rect = document.createElement('div');
+        document.querySelector('.demo-container').appendChild(rect);
+        rect.classList.add('rect');
+        rect.style.width = w + 'px';
+        rect.style.height = h + 'px';
+        rect.style.left = (img.offsetLeft + x) + 'px';
+        rect.style.top = (img.offsetTop + y) + 'px';
+      };
+    };
